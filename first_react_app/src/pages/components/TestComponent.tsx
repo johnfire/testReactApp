5e5792e273596c123fe020a2e818832570c61150
christopher rehm <christopherrehm@web.de>
2023-03-24 16:06:29 +0100

    added a test component

---
+++ first_react_app/src/pages/components/TestComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const NewTestComponent = () => {
+    console.log('reached the test component ')
+    return (
+
+    <>
+        <div> my test component what is going on here where is the text </div>
+    </>
+    )
+}
+
+
+export default  NewTestComponent
